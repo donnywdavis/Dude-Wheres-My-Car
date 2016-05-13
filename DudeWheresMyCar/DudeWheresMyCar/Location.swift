7ebd4ed1232fec3f34380500a312b689dba41834
Donny Davis <donnywdavis@icloud.com>
2016-05-13 16:12:24 -0400

    Set current location and save location in NSUserDefaults

--- DudeWheresMyCar/DudeWheresMyCar/Location.swift
+++ DudeWheresMyCar/DudeWheresMyCar/Location.swift
@@ -7,13 +7,36 @@
 //
 
 import UIKit
+import MapKit
 
-class Location: NSObject {
-    var latitude: Double?
-    var longitude: Double?
+class Location: NSObject, MKAnnotation, NSCoding {
+    var title: String?
+    var subtitle: String?
+    var coordinate: CLLocationCoordinate2D
     
-    init(latitude: Double, longitude: Double) {
-        self.latitude = latitude
-        self.longitude = longitude
+    init(coordinate: CLLocationCoordinate2D, title: String, subtitle: String) {
+        self.coordinate = coordinate
+        self.title = title
+        self.subtitle = subtitle
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        guard let latitude = aDecoder.decodeObjectForKey("latitude") as? CLLocationDegrees,
+            let longitude = aDecoder.decodeObjectForKey("longitude") as? CLLocationDegrees,
+            let title = aDecoder.decodeObjectForKey("title") as? String,
+            let subtitle = aDecoder.decodeObjectForKey("subtitle") as? String else {
+                return nil
+        }
+        
+        let coordinate = CLLocationCoordinate2DMake(latitude, longitude)
+        
+        self.init(coordinate: coordinate, title: title, subtitle: subtitle)
+    }
+    
+    func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeObject(coordinate.latitude, forKey: "latitude")
+        aCoder.encodeObject(coordinate.longitude, forKey: "longitude")
+        aCoder.encodeObject(title, forKey: "title")
+        aCoder.encodeObject(subtitle, forKey: "subtitle")
     }
 }
--- DudeWheresMyCar/DudeWheresMyCar/ViewController.swift
+++ DudeWheresMyCar/DudeWheresMyCar/ViewController.swift
@@ -9,21 +9,101 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    //
+    // MARK: Properties
+    //
+    
+    weak var appDelegate = UIApplication.sharedApplication().delegate as? AppDelegate
     
     @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    let mapRegionRadius: CLLocationDistance = 500
+//    let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
+    
+    
+    //
+    // MARK: View Life Cycle
+    //
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        checkLocationAuthorization()
+        
+        let storedLocation = retrieveLocation()
+        
+        if let storedLocation = storedLocation {
+            mapView.addAnnotation(storedLocation)
+            centerMapOnLocation(storedLocation)
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    //
+    // MARK: Button Actions
+    //
 
-    @IBAction func currentLocation(sender: UIBarButtonItem) {
+    @IBAction func getCurrentLocation(sender: UIBarButtonItem) {
+        locationManager.startUpdatingLocation()
+        guard let mapViewUserLocation = mapView.userLocation.location else {
+            locationManager.stopUpdatingLocation()
+            return
+        }
+        
+        let currentLocation: Location = Location(coordinate: CLLocationCoordinate2DMake(mapViewUserLocation.coordinate.latitude, mapViewUserLocation.coordinate.longitude), title: "Current Location", subtitle: "")
+        mapView.addAnnotation(currentLocation)
+        centerMapOnLocation(currentLocation)
+        saveLocation(currentLocation)
+        locationManager.stopUpdatingLocation()
+    }
+    
+    
+    //
+    // MARK: Map Helper Functions
+    //
+    
+    func checkLocationAuthorization() {
+        if CLLocationManager.authorizationStatus() == .AuthorizedWhenInUse {
+            mapView.showsUserLocation = true
+        } else {
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    func centerMapOnLocation(location: Location) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, mapRegionRadius * 2.0, mapRegionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    
+    //
+    // MARK: User Defaults
+    //
+    
+    func saveLocation(location: Location) {
+        let data = NSKeyedArchiver.archivedDataWithRootObject(location)
+        appDelegate?.defaults.setObject(data, forKey: "storedLocation")
+    }
+    
+    func retrieveLocation() -> Location? {
+        if let storedLocationData = appDelegate?.defaults.objectForKey("storedLocation") as? NSData {
+            let storedLocation = NSKeyedUnarchiver.unarchiveObjectWithData(storedLocationData)
+            guard let coordinate = storedLocation?.coordinate, let title = storedLocation?.title, let subtitle = storedLocation?.subtitle else {
+                return nil
+            }
+            
+            return Location(coordinate: coordinate, title: title, subtitle: subtitle)
+        }
+        
+        return nil
     }
 
 }
